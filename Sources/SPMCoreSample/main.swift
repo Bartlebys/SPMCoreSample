d73d14d88df05235087cba4dad1bc59b59699a1e
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2017-12-08 18:49:16 +0100

    Basic sample skeleton is compilable

--- Sources/SPMCoreSample/main.swift
+++ Sources/SPMCoreSample/main.swift
@@ -1,18 +1,36 @@
-import BartlebysCore
+
+import Foundation
+#if os(iOS)
+    import BartlebysCoreiOS
+#elseif os(macOS)
+    import BartlebysCore
+#elseif os(Linux)
+    import BartlebysCore
+#endif
 
 // @todo implement a full DataPoint sample
-class  MyDataPoint: DataPoint {
-     /*
-    override open var authenticationMethod: AuthenticationMethod = .basicHTTPAuth
-    override open var scheme: Scheme = Scheme.https
-    override open var host: String = "demo.bartlebys.org"
-    override open var apiBasePath: String = "www/api/v1/"
+open class  MyDataPoint: DataPoint {
+
+    public required init(credentials: Credentials) {
+        super.init(credentials: credentials)
+        self._loadCollections()
+    }
     override open func integrateResponse<T>(_ response: Response<T>) where T : Decodable, T : Encodable {
     }
-    */
+    private func _loadCollections() {
+    }
  }
 
-let credentials = Credentials(username: "NoBody", password: "NoBody", playerId: "")
-let session = Session(sessionDelegate: MyDataPoint(credentials: credentials))
 
-print (session.apiBasePath)
+// Create a data Data Point with specific Credentials.
+let credentials = Credentials(username: "NoBody", password: "NoBody")
+let dataPoint = MyDataPoint(credentials: credentials)
+dataPoint.authenticationMethod = .basicHTTPAuth
+dataPoint.scheme = Schemes.https
+dataPoint.host = "demo.bartlebys.org"
+dataPoint.apiBasePath = "www/api/v1/"
+
+// Create a new session
+let session = Session(sessionDelegate: dataPoint)
+
+
