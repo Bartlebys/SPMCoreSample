f8fe9f58203f35f793b2f68ad3e15dfe635c9763
Benoit Pereira da Silva <benoit@pereira-da-silva.com>
2017-12-11 15:24:19 +0100

    Updated to support v1.0.4

--- Sources/SPMCoreSample/main.swift
+++ Sources/SPMCoreSample/main.swift
@@ -11,11 +11,8 @@ import Foundation
 // @todo implement a full DataPoint sample
 open class  MyDataPoint: DataPoint {
 
-    public required init(credentials: Credentials) {
-        super.init(credentials: credentials)
-    }
-
-    override open func integrateResponse<T>(_ response: Response<T>){
+    required public init(credentials: Credentials, sessionIdentifier: String, coder: ConcreteCoder) throws {
+        try super.init(credentials: credentials, sessionIdentifier: sessionIdentifier, coder: coder)
     }
 
  }
@@ -23,17 +20,22 @@ open class  MyDataPoint: DataPoint {
 // Use mongo default
 MODELS_PRIMARY_KEY = Model.ModelCodingKeys._id
 
-
 let credentials = Credentials(username: "NoBody", password: "NoBody")
 
-// Create a data Data Point with specific initial Credentials.
-let dataPoint = MyDataPoint(credentials: credentials)
-dataPoint.authenticationMethod = .basicHTTPAuth
-dataPoint.scheme = Schemes.https
-dataPoint.host = "demo.bartlebys.org"
-dataPoint.apiBasePath = "www/api/v1/"
+do{
 
-// Create a new session
-let session = Session(sessionDelegate: dataPoint)
+    // Create a data Data Point with specific initial Credentials.
+    let dataPoint = try MyDataPoint(credentials: credentials, sessionIdentifier: "xxx", coder: JSONCoder())
+    dataPoint.authenticationMethod = .basicHTTPAuth
+    dataPoint.scheme = Schemes.https
+    dataPoint.host = "demo.bartlebys.org"
+    dataPoint.apiBasePath = "www/api/v1/"
+
+    /// Use the data Point
+
+
+}catch{
+    Logger.log("\(error)", category: Logger.Categories.critical)
+}
 
 
